f5d8643c18131b1fbd864410bc64fcbefe0b02ab
Tobias Stuenkel <tobias.stuenkel@gmx.de>
2022-06-29 14:58:21 +0200

    initial route configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :softwares do
+    resources :releases, only: [ :new, :create ]
+  end
+  resources :releases, only: [ :edit, :update, :destroy ] do
+    resources :reviews, only: [ :new, :create ]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
