579cea516bf0bbee4b3316aa39e4d3bd1685951b
TomLeWagon <tmarwedel@slb.com>
2022-06-29 06:50:29 -0500

    first migrate of Model review2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_114816) do
+ActiveRecord::Schema.define(version: 2022_06_29_115000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 2022_06_29_114816) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["software_id"], name: "index_releases_on_software_id"
     t.index ["user_id"], name: "index_releases_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "release_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["release_id"], name: "index_reviews_on_release_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "softwares", force: :cascade do |t|
@@ -49,4 +60,6 @@ ActiveRecord::Schema.define(version: 2022_06_29_114816) do
 
   add_foreign_key "releases", "softwares"
   add_foreign_key "releases", "users"
+  add_foreign_key "reviews", "releases"
+  add_foreign_key "reviews", "users"
 end
