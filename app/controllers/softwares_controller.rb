9a9d6027e91579d017d155ae928e8e128bd87035
Markus Koenig <mkoenig2@onesubsea.slb.com>
2022-07-04 14:09:01 +0200

    Authorize software index page

--- app/controllers/softwares_controller.rb
+++ app/controllers/softwares_controller.rb
@@ -1,6 +1,6 @@
 class SoftwaresController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
   def index
-    @softwares = Software.all
+    @softwares = policy_scope(Software)
   end
 end
