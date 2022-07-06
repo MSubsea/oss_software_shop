493373671953ccd68642607cda1789bd0e9d0c25
Markus Koenig <mkoenig2@onesubsea.slb.com>
2022-07-06 14:59:38 +0200

    Add create release route

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -4,4 +4,21 @@ class ReleasesController < ApplicationController
     authorize @release
     @software = Software.find(params[:software_id])
   end
+
+  def create
+    @release = Release.new(release_params)
+    authorize @release
+    @software = Software.find(params[:software_id])
+    @release.user = current_user
+    @release.software = @software
+    @release.save!
+    redirect_to software_path(@software)
+  end
+
+  private
+
+  def release_params
+    params.require(:release).permit(:version_number, :software_url)
+  end
+
 end
