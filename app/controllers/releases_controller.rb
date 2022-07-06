d7c9366ab9a88d0e14fec1f89b6fcdab28650140
Markus Koenig <mkoenig2@onesubsea.slb.com>
2022-07-06 14:50:23 +0200

    Add new release route

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,2 +1,7 @@
 class ReleasesController < ApplicationController
+  def new
+    @release = Release.new
+    authorize @release
+    @software = Software.find(params[:software_id])
+  end
 end
--- app/policies/release_policy.rb
+++ app/policies/release_policy.rb
@@ -5,4 +5,8 @@ class ReleasePolicy < ApplicationPolicy
     #   scope.all
     # end
   end
+
+  def create?
+    true
+  end
 end
