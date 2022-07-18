9ffea5e8d4f7f0c1211b7502dd59a71165b9c934
Benjamin Stephany <b.stephany@gmx.de>
2022-07-18 16:31:50 +0200

    correct user policy for customers

--- app/policies/customer_policy.rb
+++ app/policies/customer_policy.rb
@@ -2,7 +2,7 @@ class CustomerPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      if user.admin?
+      if user.present?
         scope.all
       end
     end
