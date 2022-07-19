3c1a36a646221ee3eceda227dbe47a342719e93b
Markus Koenig <markus.subsea@gmail.com>
2022-07-19 08:51:53 +0200

    Correct Update SW Form and Access rights

--- app/policies/customer_policy.rb
+++ app/policies/customer_policy.rb
@@ -2,7 +2,7 @@ class CustomerPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      if user.admin?
+      if user
         scope.all
       end
     end
