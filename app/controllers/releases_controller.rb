f9feaa6f39c80b79e8ed2e349d6cda8a0409520c
Bjoern Wiegert <bwiegert@slb.com>
2022-07-22 20:04:17 +0200

    Change Style of the Page

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -5,6 +5,11 @@ class ReleasesController < ApplicationController
     @software = Software.find(params[:software_id])
   end
 
+  def show
+    @release = Release.find(params[:id])
+    authorize @release
+  end
+
   def create
     @release = Release.new(release_params)
     authorize @release
